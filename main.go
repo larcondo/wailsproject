8c7bb0a9ff864dcb30df1d98ebf1b299940cdade
larcondo <larcondo90@gmail.com>
2025-02-16 19:39:13 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"embed"
+	// "runtime"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	AppMenu := menu.NewMenu()
+	FileMenu := AppMenu.AddSubmenu("File")
+	FileMenu.AddText("Minimizar", keys.CmdOrCtrl("o"), func(cd *menu.CallbackData) {
+		runtime.WindowMinimise(app.ctx)
+	})
+	FileMenu.AddSeparator()
+	FileMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(cd *menu.CallbackData) {
+		runtime.Quit(app.ctx)
+	})
+
+	// if runtime.GOOS == "darwin" {
+	// 	AppMenu.Append(menu.EditMenu()) // on macos platform, we should append EditMenu to enable Cmd+C,Cmd+V,Cmd+Z... shortcut
+	// }
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "wailsproject",
+		Width:  800,
+		Height: 600,
+		Menu:   AppMenu,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
