320f312bc06a9ce4e19dfeb75c4be4fa105edf5c
larcondo <larcondo90@gmail.com>
2025-02-19 23:27:10 -0300

    add priority summary section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import {
   CreateTodo,
@@ -15,10 +15,20 @@ export const PRIORITIES = [
   { class: 'pri-high', text: 'Alta' },
 ];
 
+type TodoArray = Array<main.TodoEntry>;
+
+function filterTodosByPri(todos: TodoArray, pri: number) {
+  return todos.filter((t) => t.Priority === pri);
+}
+
 function App() {
   const [text, setText] = useState('');
   const [priority, setPriority] = useState(0);
-  const [todos, setTodos] = useState<Array<main.TodoEntry>>([]);
+  const [todos, setTodos] = useState<TodoArray>([]);
+  const infoTodos = useMemo(() => filterTodosByPri(todos, 0), [todos]);
+  const lowTodos = useMemo(() => filterTodosByPri(todos, 1), [todos]);
+  const highTodos = useMemo(() => filterTodosByPri(todos, 2), [todos]);
+
   const updateTodo = (e: any) => setText(e.target.value);
   const updatePriority = (e: any) => setPriority(parseInt(e.target.value));
 
@@ -55,32 +65,54 @@ function App() {
     <div id="App">
       <h1>To-do List</h1>
 
-      <div id="input" className="input-box">
-        <input
-          id="new-todo"
-          className="input"
-          onChange={updateTodo}
-          autoComplete="off"
-          name="new-todo"
-          type="text"
-          value={text}
-        />
-        <select
-          name="priority"
-          id="priority"
-          value={priority}
-          onChange={updatePriority}
-        >
-          <option value={0}>{PRIORITIES[0].text}</option>
-          <option value={1}>{PRIORITIES[1].text}</option>
-          <option value={2}>{PRIORITIES[2].text}</option>
-        </select>
-        <button className="btn" onClick={createTodo}>
-          Add
-        </button>
-      </div>
+      <div className="layout">
+        <div className="priority-section">
+          <div className="priority-row">
+            <span className="priority-text">Alta</span>{' '}
+            <span className="qty-number">{highTodos.length}</span>
+          </div>
+          <div className="priority-row">
+            <span className="priority-text">Baja</span>{' '}
+            <span className="qty-number">{lowTodos.length}</span>
+          </div>
+          <div className="priority-row">
+            <span className="priority-text">Info</span>{' '}
+            <span className="qty-number">{infoTodos.length}</span>
+          </div>
+        </div>
 
-      <TodoList todos={todos} Delete={deleteTodo} Update={updateCompleted} />
+        <div>
+          <div id="input" className="input-box">
+            <input
+              id="new-todo"
+              className="input"
+              onChange={updateTodo}
+              autoComplete="off"
+              name="new-todo"
+              type="text"
+              value={text}
+            />
+            <select
+              name="priority"
+              id="priority"
+              value={priority}
+              onChange={updatePriority}
+            >
+              <option value={0}>{PRIORITIES[0].text}</option>
+              <option value={1}>{PRIORITIES[1].text}</option>
+              <option value={2}>{PRIORITIES[2].text}</option>
+            </select>
+            <button className="btn" onClick={createTodo}>
+              Add
+            </button>
+          </div>
+          <TodoList
+            todos={todos}
+            Delete={deleteTodo}
+            Update={updateCompleted}
+          />
+        </div>
+      </div>
     </div>
   );
 }
